bbeb197819c327c8ee6adbc144978f83b9831260
Qurban Yusufov <qurbanyusifov89@gmail.com>
2024-08-13 11:38:05 +0400

    function added

--- app/src/main/java/com/example/gitlearning2/MainActivity.kt
+++ app/src/main/java/com/example/gitlearning2/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(hesabla())
+
+    }
+
+
+
+    fun hesabla():Int{
+        return 10
     }
 }
